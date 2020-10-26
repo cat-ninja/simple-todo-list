7dcce30bf365e167e0f551262700ea5c496bbc0a
cat-ninja <17785134+cat-ninja@users.noreply.github.com>
2020-10-26 12:59:13 +0300

    Items endpoint & todos load when DOM is ready

--- public/javascripts/items.js
+++ public/javascripts/items.js
@@ -1,5 +1,11 @@
 const todoInput = document.querySelector(".todo-text");
 
+$(window).on("DOMContentLoaded", async () => {
+  const response = await fetch("/items");
+  const items = await response.json();
+  items.forEach((i) => appendTodoElement(i.value));
+});
+
 $(todoInput).on("keyup", (e) => {
   if (e.key === "Enter") addItem();
 });
@@ -9,18 +15,23 @@ $(".add-item").on("click", () => {
 });
 
 function addItem() {
+  let todoItem = todoInput.value;
+  if (todoItem.length > 0) {
+    appendTodoElement(todoItem);
+  } else {
+    alert("Please enter a title for your TODO item!");
+  }
+}
+
+function appendTodoElement(value) {
   const a = document.createElement("a");
   a.classList.add("list-group-item", "list-group-item-action");
-
   const div = document.createElement("div");
   div.classList.add("d-flex", "w-100", "justify-content-between");
-
   const h5 = document.createElement("h5");
   h5.classList.add("mb-1");
-  h5.innerText = todoInput.value;
-
+  h5.innerText = value;
   const small = document.createElement("small");
-
   div.appendChild(h5);
   div.appendChild(small);
   a.appendChild(div);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/todo');
+router.get("/", (req, res, next) => {
+  res.redirect("/todo");
 });
 
-router.get('/todo', function(req, res, next) {
-  res.render('todo');
+router.get("/todo", (req, res, next) => {
+  res.render("todo");
+});
+
+router.get("/items/", (req, res, next) => {
+  res.send([{ value: "this is my todo item" }]);
 });
 
 module.exports = router;
