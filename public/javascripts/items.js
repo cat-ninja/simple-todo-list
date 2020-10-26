c0623c1fd6dfda27f6d62be710dfbb33f643742d
cat-ninja <17785134+cat-ninja@users.noreply.github.com>
2020-10-26 18:39:30 +0300

    Code for templated rendering & styles. New items are on top of the list.

--- public/javascripts/items.js
+++ public/javascripts/items.js
@@ -1,10 +1,10 @@
 const todoInput = document.querySelector(".todo-text");
 
-$(window).on("DOMContentLoaded", async () => {
-  const response = await fetch("/items");
-  const items = await response.json();
-  items.forEach((i) => appendTodoElement(i.value));
-});
+// $(window).on("DOMContentLoaded", async () => {
+//   const response = await fetch("/items");
+//   const items = await response.json();
+//   items.forEach((i) => appendTodoElement(i.value));
+// });
 
 $(todoInput).on("keyup", (e) => {
   if (e.key === "Enter") addItem();
@@ -24,17 +24,32 @@ function addItem() {
 }
 
 function appendTodoElement(value) {
+  // Create HTML elements
   const a = document.createElement("a");
-  a.classList.add("list-group-item", "list-group-item-action");
   const div = document.createElement("div");
-  div.classList.add("d-flex", "w-100", "justify-content-between");
+  const itemDiv = document.createElement("div");
+  const checkerDiv = document.createElement("div");
   const h5 = document.createElement("h5");
-  h5.classList.add("mb-1");
-  h5.innerText = value;
   const small = document.createElement("small");
-  div.appendChild(h5);
-  div.appendChild(small);
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  // Assign desired classes
+  a.classList.add("list-group-item", "list-group-item-action");
+  div.classList.add("d-flex", "w-100", "justify-content-between");
+  itemDiv.classList.add("item");
+  checkerDiv.classList.add("checker", "d-flex", "align-items-center");
+  h5.classList.add("mb-1");
+  // Set the text of the element
+  h5.innerText = value;
+  small.innerText = "today";
+  // Construct elements tree
+  itemDiv.appendChild(h5);
+  itemDiv.appendChild(small);
+  checkerDiv.appendChild(checkbox);
+  div.appendChild(itemDiv);
+  div.appendChild(checkerDiv);
   a.appendChild(div);
-  document.querySelector(".list-group").appendChild(a);
+  document.querySelector(".list-group").prepend(a);
+  // Flush the input
   todoInput.value = "";
 }
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,19 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/todo", (req, res, next) => {
-  res.render("todo");
+  res.render("todo", {
+    items: [
+      { name: "this is my todo item", date: "1 day ago" },
+      { name: "this is my second todo item", date: "2 days ago" },
+    ],
+  });
 });
 
 router.get("/items/", (req, res, next) => {
-  res.send([{ value: "this is my todo item" }]);
+  res.send([
+    { value: "this is my todo item" },
+    { value: "this is my second todo item" },
+  ]);
 });
 
 module.exports = router;
